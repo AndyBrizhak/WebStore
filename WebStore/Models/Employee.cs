4f9d490700754497c7d413350163d92db2825731
Андрей Брижак <andrewbrizhak@gmail.com>
2020-08-23 17:09:12 +0800

    add model Employee add model DbConext

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    /// <summary>
+    /// Model Employee
+    /// </summary>
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+    }
+}
