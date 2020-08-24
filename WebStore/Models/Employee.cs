5687ed1fd33157a4a0b62854e816faf4732cc563
Андрей Брижак <andrewbrizhak@gmail.com>
2020-08-24 08:49:22 +0800

    cancelled add new propertys on Employee model

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -19,8 +19,7 @@ namespace WebStore.Models
         public string SurName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
-        public DateTime BirthDay { get; set; }
-        public DateTime DateEmployment { get; set; }
+        
 
     }
 }
