3d7ad280e051443de04d4a8b52a442d91af898fb
Андрей Брижак <andrewbrizhak@gmail.com>
2020-08-24 08:31:53 +0800

    add property model Employee

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -19,5 +19,8 @@ namespace WebStore.Models
         public string SurName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime DateEmployment { get; set; }
+
     }
 }
